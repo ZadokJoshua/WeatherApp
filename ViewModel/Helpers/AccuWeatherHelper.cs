f1c7774abccd8d1dacc076f9ac12f8943f430e90
Zadok Joshua <ayegbazadok@outlook.com>
2022-07-13 09:58:08 +0100

    Add project files.

---
+++ ViewModel/Helpers/AccuWeatherHelper.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Model;
+
+namespace WeatherApp.ViewModel.Helpers
+{
+    // Helpers are usually APIs
+    public class AccuWeatherHelper
+    {
+        public const string BASE_URL = "http://dataservice.accuweather.com/";
+        public const string AUTO_COMPLETE_ENDPOINT = "locations/v1/cities/autocomplete?apikey={0}&q={1}";
+        public const string CURRENT_CONDITIONS_ENDPOINT = "currentconditions/v1/{0}?apikey={1}";
+        // {0} and {1} are placeholders
+
+        public const string API_KEY = "uFR7VzjfDpB2NZIKWZrtEKIUNBgnl6Ob";
+
+        public static async List<City> GetCities(string query)
+        {
+            List<City> cities = new List<City>();
+
+            string url = BASE_URL + string.Format(AUTO_COMPLETE_ENDPOINT, API_KEY, query);
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+
+                cities = JsonConvert.DeserializeObject<>(json);
+            }
+
+            return cities;
+        }
+
+    }
+}
